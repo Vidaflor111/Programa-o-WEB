6aef2dc89d1b333b193c0d19e115fd0ae3c7eb4f
Vidaflor Ferreira Costa <vidaflor.costa@estudante.ufla.br>
2023-11-07 22:28:25 -0300

    back

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model
 {
+    use CrudTrait;
     use HasFactory;
+
+    protected $table = 'eventos';
+    protected $guarded = ['id'];
+    protected $fillable = ['titulo', 'descricao', 'cidade','privado', 'image'];
 }
